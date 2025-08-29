0ccd3327511ac32a148330592faf1cc302062886
Mickieee <jaatupoj.tuan@mail.kmutt.ac.th>
2025-08-30 00:50:23 +0700

    Set up

---
+++ project/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className='flex flex-col justify-center items-center text-center font-bold text-6xl h-screen '>
+      <div>
+        จงสรรเสริญพระเจ้า อย่างกู
+      </div>
+    </div>
+  )
+}
+
+export default App
